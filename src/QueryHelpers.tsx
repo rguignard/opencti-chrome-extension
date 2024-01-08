54edf445b4df1c5ff7060fb375b3bacb437cfbee
Romain GUIGNARD <rom1.guignard@gmail.com>
2024-01-08 19:51:02 +0100

    support of CVE enrichment

--- src/QueryHelpers.tsx
+++ src/QueryHelpers.tsx
@@ -123,6 +123,129 @@ export const searchIndicator = async (observable: any, storage: any) => {
     return resp
 }
 
+export const searchVulnerability = async (observable: any, storage: any) => {
+    let query = {
+        query: `query  {
+          vulnerabilities(filters: { 
+                mode :and,
+                filters:[
+                  {
+                    key: "name",
+                    values: ["` + observable.value + `"],
+                    operator: eq,
+                    mode: or
+                  }
+                ],
+                filterGroups:[]
+              }
+           ) {
+          edges {
+            node {
+              name
+              id
+              objectLabel {
+                edges {
+                  node {
+                    id
+                    value
+                    color
+                  }
+                }
+              } 
+              stixCoreRelationships{
+                edges{
+                  node{
+                    to{
+                      ... on Campaign {
+                          id
+                          name
+                          entity_type
+                      }
+                      ... on Indicator {
+                          id
+                          name
+                          entity_type
+                      }
+                      ... on Infrastructure {
+                          id
+                          name
+                          entity_type
+                      }
+                      ... on IntrusionSet {
+                          id
+                          name
+                          entity_type
+                      }
+                      ... on Malware {
+                          id
+                          name
+                          entity_type
+                      }
+                      ... on ThreatActorGroup {
+                          id
+                          name
+                          entity_type
+                      }
+                      ... on ThreatActorIndividual {
+                          id
+                          name
+                          entity_type
+                      }
+                      ... on ThreatActor {
+                          id
+                          name
+                          entity_type
+                      }
+                      ... on Tool {
+                          id
+                          name
+                          entity_type
+                      }
+                      ... on Vulnerability {
+                          id
+                          name
+                          entity_type
+                      }
+                      ... on Incident {
+                          id
+                          name
+                          entity_type
+                      }
+                      ... on Event {
+                          id
+                          name
+                          entity_type
+                      }
+                      ... on Case {
+                          id
+                          name
+                          entity_type
+                      }
+                    }
+                  }
+                }
+              }
+              reports{
+                edges{
+                  node{
+                    id
+                    name
+                  }
+                }
+              }
+            }
+          }
+         }
+        }`
+    };
+    let resp = await sendGraphQLRequest(storage['opencti_url'], storage['opencti_token'], query);
+    if (resp && resp.hasOwnProperty("errors")) {
+        console.log(resp['errors']);
+        throw Error(resp['errors']['message']);
+    }
+    return resp
+}
+
 export const healthCheck = async (url: string, token: string) => {
     let query = {
         query: `query  {
